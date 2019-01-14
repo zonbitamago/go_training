7b25bd6a3ae0f369765ae4ed1e859288569c709b
kazuma takiuchi <basstrombone413@gmail.com>
2019-01-15 05:41:47 +0900

    add numeric_constants

---
+++ 02_tour_of_go/01_basics/16_numeric_constants/numeric_constants.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+const (
+	// Big 2進数。1の右に0が100個(2の100乗)
+	Big = 1 << 100
+	// Small Bigの0を99個削除(2の1乗)
+	Small = Big >> 99
+)
+
+func needInt(x int) int {
+	return x*10 + 1
+}
+
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
+
+func main() {
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+}
---
+++ 02_tour_of_go/01_basics/16_numeric_constants/numeric_constants.md
@@ -0,0 +1,7 @@
+# numeric_constants
+
+[https://go-tour-jp.appspot.com/basics/16](https://go-tour-jp.appspot.com/basics/16)
+
+数値の定数は、高精度の値となる。
+
+(intは64-bitの整数を保持できるが、それでは足りない場合がある。その場合にconstを活用する。)
