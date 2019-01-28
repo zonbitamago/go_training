f675912fb728576c57f2ecf499e988e6bd42b934
kazuma takiuchi <basstrombone413@gmail.com>
2019-01-28 21:37:29 +0900

    add pointers_and_functions

---
+++ 02_tour_of_go/04_methods/05_pointers_and_functions/pointers_and_functions.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Vertex 頂点
+type Vertex struct {
+	X, Y float64
+}
+
+// Abs 斜辺
+func Abs(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+// Scale 倍数
+func Scale(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{3, 4}
+	Scale(&v, 10)
+	fmt.Println(Abs(v))
+}
---
+++ 02_tour_of_go/04_methods/05_pointers_and_functions/pointers_and_functions.md
@@ -0,0 +1,5 @@
+# pointers_and_functions
+
+[https://go-tour-jp.appspot.com/methods/5](https://go-tour-jp.appspot.com/methods/5)
+
+ポインタレシーバも通常のレシーバと同様、引数にレシーバ変数を渡すことで関数で記述することができる。
